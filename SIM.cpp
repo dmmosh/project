82ad6355f34361d05861361c6641906854965c3d
wettestsock <evilsharkeater@gmail.com>
2025-03-20 12:49:40 -0400

    nothing of note

--- SIM.cpp
+++ SIM.cpp
@@ -86,7 +86,7 @@ class cache{
     }
 
     inline long long index(const long long value){
-        return (value >> 5);
+        return (value >> 5) & (1<<((int)log2(this->set_size)))-1;
     }
 
     inline long long offset(const long long value){
