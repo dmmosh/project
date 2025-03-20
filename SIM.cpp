64e1962dbc1265633eb0dd4adf2f9e35dbd4e5db
wettestsock <evilsharkeater@gmail.com>
2025-03-20 13:30:12 -0400

    nothing of note

--- SIM.cpp
+++ SIM.cpp
@@ -173,7 +173,7 @@ class cache{
 
         // if dirty to-be-evicted bit is on
         // only ever on if write-back 
-        if (this->wb == WRITE_BACK && this->cache_arr[mem_index][0].dirty == 1){
+        if (this->cache_arr[mem_index][0].dirty == 1){
             this->mem_writes++;
         }
 
